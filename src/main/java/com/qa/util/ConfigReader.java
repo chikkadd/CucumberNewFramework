e9c2ea39725cb399b8157877d4449841740fbc6c
Chikke <chikkadd20@gmail.com>
2026-02-13 22:14:43 +0530

    small changes again to pipeline

--- src/main/java/com/qa/util/ConfigReader.java
+++ src/main/java/com/qa/util/ConfigReader.java
@@ -1,30 +1,25 @@
 package com.qa.util;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-private Properties prop;
-	
-	public Properties init_prop()
-	{
-		prop= new Properties();
-		try
-		{
-			FileInputStream it=new FileInputStream("C:/Users/chikk/eclipse-workspace/CucumberNewFramework/src/test/resources/config/config.properties");
-			prop.load(it);
-		}
-		catch (FileNotFoundException e) {
+	private Properties prop;
 
-			e.printStackTrace();
-		}
-		catch (IOException e) {
+	public Properties init_prop() {
+		prop = new Properties();
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream("config/config.properties")) {
+
+			if (input == null) {
+				throw new RuntimeException("Unable to find config.properties in resources/config/");
+			}
+
+			prop.load(input);
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return prop;
-		
 	}
-
 }
